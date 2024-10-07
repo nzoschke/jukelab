c9b2d0b6acd9bd8dea78658669b2fe3b00c9cd54
Noah Zoschke <noah@mixable.net>
2024-10-07 15:12:38 -0700

    daisy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 } satisfies Config;
