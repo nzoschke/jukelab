5c45e39eaa35155682ab251ef3b5f6a84159f3d4
Noah Zoschke <noah@mixable.net>
2024-10-07 16:29:22 -0700

    spotify audio

---
+++ src/routes/spotify/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import AudioC from "./Audio.svelte";
+  import Controls from "../audio/Controls.svelte";
+  import { Audio } from "$lib/types/audio";
+
+  let audio = $state(Audio);
+</script>
+
+<svelte:head>
+  <title>Spotify Controls</title>
+  <meta name="description" content="Spotify Controls" />
+</svelte:head>
+
+<AudioC bind:audio src="spotify:track:0UK7txy2sUR6kqvEZtx72w" />
+
+<div class="flex flex-col items-center space-y-2 p-2">
+  <Controls bind:audio />
+</div>
