3b5fb6a305d27422533a553b3806fb493c32ee77
Noah Zoschke <noah@mixable.net>
2024-10-07 15:53:32 -0700

    audio src

---
+++ src/routes/audio/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Audio from "./Audio.svelte";
+</script>
+
+<svelte:head>
+  <title>Audio Source</title>
+  <meta name="description" content="Audio source" />
+</svelte:head>
+
+<section class="flex flex-col justify-center items-center flex-[0.6]">
+  <Audio class="w-full" controls loop src="/steps.mp3" />
+</section>
