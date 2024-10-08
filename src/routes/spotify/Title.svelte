8e558416c1cc571216d111d1c514fe05930b6dad
Noah Zoschke <noah@mixable.net>
2024-10-08 06:18:10 -0700

    art ui

--- src/routes/spotify/Title.svelte
+++ src/routes/spotify/Title.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
-  import type { Track } from "$lib/types/music";
+  import type { Album, Track } from "$lib/types/music";
 
   let {
+    album,
     track,
   }: {
+    album: Album;
     track: Track;
   } = $props();
 </script>
 
+{#snippet art(album?: Album)}
+  {#if album && album.art != ""}
+    <img alt="" class="h-full aspect-square" src={album.art} />
+  {:else}
+    <div class="h-full aspect-square"></div>
+  {/if}
+{/snippet}
+
 <div
-  class="flex h-24 min-h-24 w-1/2 flex-col items-center justify-center rounded bg-base-200"
+  class="flex h-24 min-h-24 w-1/2 items-center justify-center rounded bg-base-200 p-2 space-x-2"
   class:skeleton={!track.src}
 >
-  <div>{track.title}</div>
-  <div>{track.album} {track.year.getMilliseconds() == 0 ? "" : `(${track.year})`}</div>
-  <div>{track.artist}</div>
+  {@render art(album)}
+  <div class="flex flex-col items-center text-center">
+    <div>{track.title}</div>
+    <div>{track.album} {track.year.getMilliseconds() == 0 ? "" : `(${track.year})`}</div>
+    <div>{track.artist}</div>
+  </div>
+  {@render art()}
 </div>
