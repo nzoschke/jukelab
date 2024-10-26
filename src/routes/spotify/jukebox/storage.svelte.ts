165babda1db5b59f7e8ce9382cdbcd8f4b012049
Noah Zoschke <noah@mixable.net>
2024-10-26 14:39:13 +0900

    refactor

--- src/routes/spotify/jukebox/storage.svelte.ts
+++ src/routes/spotify/jukebox/storage.svelte.ts
@@ -8,6 +8,8 @@ export const Storage = () => {
       ["Jukelab 102", "spotify:playlist:3ENY9f8zKVYOegYWNJYAYV"],
     ],
   };
+  let hash: Record<string, string> = {};
+
   let playlist = "";
   let playlists: string[][] = $state([]);
 
@@ -16,24 +18,26 @@ export const Storage = () => {
     return i ? JSON.parse(i) : defaults[key];
   };
 
+  const _hash = () => {
+    var h = window.location.hash.substring(1);
+    var params: Record<string, string> = {};
+    h.split("&").map((hk) => {
+      let parts = hk.split("=");
+      params[parts[0]] = parts[1];
+    });
+
+    hash = params;
+    history.replaceState(null, "", " ");
+  };
+
   const get = () => {
+    _hash();
     playlist = _get("playlist");
     playlists = _get("playlists");
   };
 
   const getItem = (key: string) => {
-    return hash()[key] || _get(key);
-  };
-
-  const hash = () => {
-    var hash = window.location.hash.substring(1);
-    var params: Record<string, string> = {};
-    hash.split("&").map((hk) => {
-      let parts = hk.split("=");
-      params[parts[0]] = parts[1];
-    });
-
-    return params;
+    return hash[key] || _get(key);
   };
 
   const set = () => {
@@ -43,7 +47,6 @@ export const Storage = () => {
 
   const setPlaylist = (pl: Playlist) => {
     playlist = pl.src;
-    // playlists.indexOf((p) => p[1] == pl.src)
     const i = playlists.findIndex((p) => p[1] == pl.src);
     if (i >= 0) {
       playlists[i][0] = pl.title;
@@ -62,14 +65,8 @@ export const Storage = () => {
     get playlist() {
       return playlist;
     },
-    set playlist(v: string) {
-      playlist = v;
-    },
     get playlists() {
       return playlists;
-    },
-    set playlists(v: string[][]) {
-      playlists = v;
     },
   };
 };
