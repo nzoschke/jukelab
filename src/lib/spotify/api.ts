2b8e77b07beb8707e12a8a14d6639473c965554a
Noah Zoschke <noah@mixable.net>
2024-11-20 13:54:28 -0800

    playlist tracks length and album cache

--- src/lib/spotify/api.ts
+++ src/lib/spotify/api.ts
@@ -1,10 +1,11 @@
-import type { AlbumTracks, PlaylistTracks } from "$lib/types/music";
-import * as s from "@spotify/web-api-ts-sdk";
+import * as s from "$lib/storage";
+import { AlbumTracks, type PlaylistTracks } from "$lib/types/music";
+import * as sdk from "@spotify/web-api-ts-sdk";
 import * as to from "./to";
 
 export const API = (token: () => Promise<string>) => {
   const api = async () =>
-    s.SpotifyApi.withAccessToken("", {
+    sdk.SpotifyApi.withAccessToken("", {
       access_token: await token(),
       token_type: "",
       expires_in: 3600,
@@ -19,11 +20,28 @@ export const API = (token: () => Promise<string>) => {
   };
 
   const albumTracks = async (uri: string) => {
+    let album = s.get(uri, AlbumTracks);
+    if (album.src != "") {
+      let text = JSON.stringify(album);
+      const re = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/; // startswith: 2015-04-29T22:06:55
+      album = JSON.parse(text, (k, v) => {
+        if (typeof v == "string" && re.test(v)) {
+          return new Date(v);
+        }
+        return v;
+      });
+
+      return album;
+    }
+
     const a = await api();
     const out = await a.albums.get(uri.split(":")[2]);
 
-    const album = to.albumTracks(out);
+    album = to.albumTracks(out);
     album.tracks = out.tracks.items.map((t) => to.track(out, t));
+
+    s.set(uri, album);
+
     return album;
   };
 
@@ -40,24 +58,37 @@ export const API = (token: () => Promise<string>) => {
     return album;
   };
 
-  const compilations = async (userUri: string): Promise<s.SimplifiedPlaylist[]> => {
+  const compilations = async (userUri: string): Promise<sdk.SimplifiedPlaylist[]> => {
     const ps = await playlists(userUri);
     return ps.filter((p) => p.description.includes("JukeLab compilation"));
   };
 
   const playlist = async (uri: string): Promise<PlaylistTracks> => {
     const a = await api();
-    const out = await a.playlists.getPlaylist(uri.split(":")[2]);
+    const id = uri.split(":")[2];
+    const out = await a.playlists.getPlaylist(id);
+
+    let ts: sdk.PlaylistedTrack<sdk.Track>[] = [];
+    let offset = 0;
+    let total = 1;
+    while (offset < total) {
+      const out = await a.playlists.getPlaylistItems(id, undefined, undefined, 50, offset);
+      total = out.total;
+      offset += out.limit;
+      ts.push(...out.items);
+    }
+
+    out.tracks.items = ts;
 
     const playlist = to.playlist(out);
     playlist.tracks = out.tracks.items.map((i) => to.track(i.track.album, i.track));
     return playlist;
   };
 
-  const playlists = async (userUri: string): Promise<s.SimplifiedPlaylist[]> => {
+  const playlists = async (userUri: string): Promise<sdk.SimplifiedPlaylist[]> => {
     const a = await api();
 
-    let ps: s.SimplifiedPlaylist[] = [];
+    let ps: sdk.SimplifiedPlaylist[] = [];
     let offset = 0;
     let total = 1;
     while (offset < total) {
@@ -76,31 +107,15 @@ export const API = (token: () => Promise<string>) => {
   ): Promise<AlbumTracks[]> => {
     const a = await api();
 
-    let ts: s.PlaylistedTrack<s.Track>[] = [];
-    let offset = 0;
-    let total = 1;
-    while (offset < total) {
-      const out = await a.playlists.getPlaylistItems(
-        playlistUri.split(":")[2],
-        undefined,
-        undefined,
-        50,
-        offset,
-      );
-      total = out.total;
-      offset += out.limit;
-      ts.push(...out.items);
-    }
-
     const out = await a.playlists.getPlaylist(playlistUri.split(":")[2]);
     const comps = await compilations(out.owner.uri);
 
     const p = await playlist(playlistUri);
     const albums: AlbumTracks[] = [];
-    for (const t of ts) {
-      const { uri } = t.track;
-      const comp = comps.find((c) => c.description.includes(uri.split(":")[2]));
-      const a = comp ? await compilation(comp.uri) : await trackAlbum(uri);
+
+    for (const t of p.tracks) {
+      const comp = comps.find((c) => c.description.includes(t.src.split(":")[2]));
+      const a = comp ? await compilation(comp.uri) : await trackAlbum(t.src);
       albums.push(a);
       if (cb) cb(a);
 
