827a01c182d1d53dc3ea0ac25b16a65778655a76
Noah Zoschke <noah@mixable.net>
2024-12-13 15:45:17 -0800

    hello world

---
+++ Apple/JukeLabTests/JukeLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  JukeLabTests.swift
+//  JukeLabTests
+//
+//  Created by Noah Zoschke on 12/13/24.
+//
+
+import Testing
+@testable import JukeLab
+
+struct JukeLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
