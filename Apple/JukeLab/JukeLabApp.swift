b97de49accb0c5150e1f1cdbc81da59e58eb6ba2
Noah Zoschke <noah@mixable.net>
2024-12-13 15:46:46 -0800

    swift-format

--- Apple/JukeLab/JukeLabApp.swift
+++ Apple/JukeLab/JukeLabApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct JukeLabApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
