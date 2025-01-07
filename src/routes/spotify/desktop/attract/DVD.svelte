f4ce52e23b0cc98f707c5c0f0d325396815328c1
Pedro Belo <pedrobelo@gmail.com>
2025-01-06 20:07:08 -0800

    dvd tweaks

--- src/routes/spotify/desktop/attract/DVD.svelte
+++ src/routes/spotify/desktop/attract/DVD.svelte
@@ -9,15 +9,12 @@
     track: { title, artist },
   } = playlist;
 
-  // Position/velocity for the DVD logo
   let x = $state(0);
   let y = $state(0);
-  let vx = 3;
-  let vy = 3;
+  let vx = 1;
+  let vy = 1;
 
-  // Logo size
-  let logoWidth = 200;
-  let logoHeight = 200;
+  const size = 200; // width and height, in px
 
   let animId: number;
   onMount(() => (animId = requestAnimationFrame(animate)));
@@ -30,10 +27,10 @@
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
-    if (x < 0 || x + logoWidth > screenWidth) {
+    if (x < 0 || x + size > screenWidth) {
       vx = -vx;
     }
-    if (y < 0 || y + logoHeight > screenHeight) {
+    if (y < 0 || y + size > screenHeight) {
       vy = -vy;
     }
 
@@ -47,7 +44,7 @@
       src={art}
       alt=""
       class="absolute"
-      style="width: {logoWidth}px; height: {logoHeight}px; left: {x}px; top: {y}px;"
+      style="width: {size}px; height: {size}px; left: {x}px; top: {y}px;"
     />
     <p class="absolute bottom-5 left-5 pt-2 text-xl italic">{title} by <b>{artist}</b></p>
   {/if}
